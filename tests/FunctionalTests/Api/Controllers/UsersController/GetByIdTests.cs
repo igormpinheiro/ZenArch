4c2db6cd8f07740e08567e58d307697a6de2569b
Igor Pinheiro <igordf@gmail.com>
2025-08-09 22:02:46 -0300

    tests: add UsersController - GetByIdTests

---
+++ tests/FunctionalTests/Api/Controllers/UsersController/GetByIdTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using Application.Models;
+using Shouldly;
+
+namespace FunctionalTests.Api.Controllers.UsersController;
+
+public class GetByIdTests(TestContainerFixture testContainerFixture) : ApiTestBase(testContainerFixture)
+{
+    private const string _usersEndpoint = "api/users";
+
+    [Fact]
+    public async Task Success()
+    {
+        // Arrange
+        var createRequest = new UserInputModel
+        {
+            Name = "Test User",
+            Email = "test@email.com"
+        };
+
+        var createResponse = await DoPostAsync(_usersEndpoint, createRequest);
+        var createdUserData = await AssertSuccessResponseAndGetDataAsync(createResponse, HttpStatusCode.Created);
+        var userId = createdUserData?.GetProperty("id").GetString();
+        userId.ShouldNotBeNullOrWhiteSpace();
+
+        // Act
+        var response = await DoGetAsync($"{_usersEndpoint}/{userId}");
+
+        // Assert
+        var userData = await AssertSuccessResponseAndGetDataAsync(response, HttpStatusCode.OK);
+
+        userData?.GetProperty("id").GetString().ShouldBe(userId);
+        userData?.GetProperty("name").GetString().ShouldBe(createRequest.Name);
+        userData?.GetProperty("email").GetString().ShouldBe(createRequest.Email);
+    }
+
+    [Fact]
+    public async Task Failure()
+    {
+        var invalidId = new Guid("00000000-0000-0000-0000-000000000001");
+
+        // Act - Attempt to get non-existing user
+        var response = await DoGetAsync($"{_usersEndpoint}/{invalidId}");
+
+        // Assert
+        await AssertErrorResponseAndGetDataAsync(response, HttpStatusCode.NotFound);
+    }
+}
